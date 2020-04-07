b471c8285257e9b6064ed5960eda502d3cd3d3ae
defpis <wenjun@datagrand.com>
2020-04-07 21:31:23 +0800

    add tcp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 你需要知道的所有有关 http 的知识
+# 你需要知道的所有有关 HTTP 的知识
+
+- [TCP 三次握手和四次挥手](https://github.com/defpis/all-about-http-that-you-should-know/blob/master/docs/TCP%E4%B8%89%E6%AC%A1%E6%8F%A1%E6%89%8B%E5%92%8C%E5%9B%9B%E6%AC%A1%E6%8C%A5%E6%89%8B.md)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import * as http from 'http';
+
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    console.log(`${req.method} ${req.url}`);
+    res.write('Hello World!');
+    res.end();
+  },
+);
+
+const host = 'localhost';
+const port = 4000;
+console.log(`Start server on ${host}:${port}`);
+server.listen(port, host);
